361424f73369d23192fb7309949ef2ff752eafe6
hakeem-jc <hakeemclarke14@gmail.com>
2025-04-29 11:26:18 -0500

    feat: Create header and remove boilerplate code

---
+++ app/components/Header.tsx
@@ -0,0 +1,87 @@
+"use client"
+import { FC, useState } from "react";
+
+const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="bg-secondary fixed w-full z-20 top-0 start-0">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="./" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="./logo.png" className="h-14" alt="Lone Logo" />
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center"
+          >
+            Get started
+          </button>
+          <button
+            onClick={toggleNavbar}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-400 rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded={isOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+            isOpen ? "flex" : "hidden"
+          }`}
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-white rounded-sm hover:text-white md:hover:bg-transparent md:p-0 md:text-white"
+              >
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-white rounded-sm hover:text-white md:hover:bg-transparent md:p-0 md:text-white"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-white rounded-sm hover:text-white md:hover:bg-transparent md:p-0 md:text-white"
+              >
+                FAQ
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
