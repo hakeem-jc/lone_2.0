61547bfe702dd875c3feba43b11e8dc69e8fbb1d
hakeem-jc <hakeemclarke14@gmail.com>
2025-05-05 11:27:06 -0500

    feat: Add login and onboarding pages

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/app/components/Header";
 
-export default function Home() {
+const Page = () => {
   return (
     <main className="pt-24">
       <Header />
       <section className="flex flex-col gap-6 p-4">
-        <h1 className="text-2xl text-center">Temp</h1>
+        <h1 className="text-2xl text-center">Sign Up</h1>
       </section>
     </main>
   );
 }
+
+export default Page;
