c626ad1bb3e1525e8739fad527004a674986720c
hakeem-jc <hclarke.dev@gmail.com>
2025-07-01 13:44:52 +0200

    feat: Add stepper component to onboarding page

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,11 +1,36 @@
 import Header from "@/app/components/Header";
+import Stepper from "@/app/components/ui/Stepper";
+
 
 const Page = () => {
   return (
     <main className="pt-24">
       <Header />
       <section className="flex flex-col gap-6 p-4">
-        <h1 className="text-2xl text-center">Onboarding</h1>
+        <Stepper
+            steps={[
+              {
+                title: "Account info",
+                description: "Tell us about yourself",
+                completed: true,
+              },
+              {
+                title: "Loan info",
+                description: "How much do you want to borrow",
+                completed: false,
+              },
+              {
+                title: "Review",
+                description: "Wrap up some paperwork",
+                completed: false,
+              },
+              {
+                title: "Disburse",
+                description: "Money in your account in seconds",
+                completed: false,
+              }
+            ]}
+          />
       </section>
     </main>
   );
