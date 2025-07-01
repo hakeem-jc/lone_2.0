16d690ba96e382106505870ecd3d4d202900da18
hakeem-jc <hclarke.dev@gmail.com>
2025-07-01 14:23:47 +0200

    feat: Update Stepper by removing light themes

--- app/components/ui/Stepper.tsx
+++ app/components/ui/Stepper.tsx
@@ -17,16 +17,12 @@ const Stepper: React.FC<StepperProps> = ({ steps }) => {
         <li
           key={index}
           className={`flex items-center ${
-            step.completed
-              ? "text-blue-600 dark:text-blue-500"
-              : "text-gray-500 dark:text-gray-400"
+            step.completed ? "text-blue-500" : "text-gray-400"
           } space-x-2.5 rtl:space-x-reverse`}
         >
           <span
             className={`flex items-center justify-center w-8 h-8 border rounded-full shrink-0 ${
-              step.completed
-                ? "border-blue-600 dark:border-blue-500"
-                : "border-gray-500 dark:border-gray-400"
+              step.completed ? "border-blue-500" : "border-gray-400"
             }`}
           >
             {index + 1}
